a9f4ee7e7679a2f2d62556c185f8ab5a8568def2
Omar <omeralhadu@gmail.com>
2022-01-21 13:43:47 +0200

    Initial Commit

---
+++ E-CommerceUI/E_CommerceUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_CommerceUIApp.swift
+//  E-CommerceUI
+//
+//  Created by Omar Abdulrahman on 1/21/22.
+//
+
+import SwiftUI
+
+@main
+struct E_CommerceUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
